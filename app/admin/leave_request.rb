65ab99bd76d80b4e2f6dfbd56d053532264f82b0
Samuel Adesoga <samueladesoga@gmail.com>
2019-05-09 23:56:14 +0100

    fixed filter in user

--- app/admin/leave_request.rb
+++ app/admin/leave_request.rb
@@ -1,12 +1,19 @@
 ActiveAdmin.register LeaveRequest do
   permit_params :approved, :commence, :end_date
 
+  batch_action :approve do |ids|
+    batch_action_collection.find(ids).each do |leave_request|
+      leave_request.update_attributes(:approved => true)
+    end
+    redirect_to collection_path, alert: "The leave requests have been approved"
+  end
+
   index do
     selectable_column
     id_column
     column :name, :sortable => :id do |leave_request|
        leave_request.patron.full_name
-     end
+    end
     column :commence
     column :end_date
     column :reason
@@ -14,7 +21,7 @@ ActiveAdmin.register LeaveRequest do
     actions
   end
 
-  filter :name
+  filter :patron_first_name_or_patron_last_name_cont, label: 'Name'
   filter :commence
   filter :end_date
   filter :reason
