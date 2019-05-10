77456fe3c3f5aca694fdff6f9dcab21dd6ab98b9
Samuel Adesoga <samueladesoga@gmail.com>
2019-05-10 09:01:15 +0100

    added config for heroku

--- config/environments/heroku.rb
+++ config/environments/heroku.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :authentication => :plain,
-      :address => 'smtp.sendgrid.net',
+      :address => 'email-smtp.us-east-1.amazonaws.com',
       :port => 587,
       :domain => ENV['SENDGRID_DOMAIN'],
       :user_name => ENV['SENDGRID_USERNAME'],
