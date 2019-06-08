90d51743f03446e19ddd304b228c72a90aae78c4
Samuel Adesoga <samueladesoga@gmail.com>
2019-06-08 21:18:40 +0100

    configured devise to send email lager

--- app/models/patron.rb
+++ app/models/patron.rb
@@ -7,4 +7,8 @@ class Patron < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
   has_many :leave_requests
 
+  def send_devise_notification(notification, *args)
+  	devise_mailer.send(notification, self, *args).deliver_later
+  end
+
 end
