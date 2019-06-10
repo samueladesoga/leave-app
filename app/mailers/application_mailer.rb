cbbce3b2fb0b5599f48c66b9734a9da04ab88f6a
Samuel Adesoga <samueladesoga@gmail.com>
2019-06-10 19:34:42 +0100

    added GH choir email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  #default from: 'gloryhousechoir@gmail.com'
-  default from: 'samueladesoga@gmail.com'
+  default from: 'gloryhousechoir@gmail.com'
   layout 'mailer'
 end
