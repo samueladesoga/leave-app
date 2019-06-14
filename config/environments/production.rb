07afbc911f93574b5ef6f4039a96602e5fb99318
Samuel Adesoga <samueladesoga@gmail.com>
2019-06-14 23:08:47 +0100

    fixed ses endpoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :authentication => :plain,
-      :address => 'email-smtp.us-east-1.amazonaws.com',
+      :address => 'email-smtp.eu-west-1.amazonaws.com',
       :port => 587,
       :domain => ENV['MAIL_DOMAIN'],
       :user_name => ENV['MAIL_USERNAME'],
